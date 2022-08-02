be7158329bb17a97fe556e39342a29248e885850
hamadamu <ryusuke1133@gmail.com>
2022-08-02 14:12:56 +0000

    タスク一覧ページ、タスクの表示完了、レイアウトから

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
 class TasksController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @tasks = @user.tasks #ユーザーのタスクが全部入ってる
+  end
+  
+  def new
+    @user = User.find(params[:user_id])
+    @task = @user.tasks
+  end
 end
