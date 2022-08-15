de7324d10fa265638c22ccdc87f8f95af0b34e24
hamadamu <ryusuke1133@gmail.com>
2022-08-15 08:15:19 +0000

    Finish C6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,22 @@ class TasksController < ApplicationController
   
   def new
     @user = User.find(params[:user_id])
-    @task = @user.tasks
+    @task = @user.tasks.new
   end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.new(task_params) # 間違えてparams[task_params]にしない
+    if @task.save
+      redirect_to user_tasks_url
+    else
+      render :new
+    end
+  end
+  
+  
+  private
+    def task_params
+      params.require(:task).permit(:name, :note, :user_id)
+    end
 end
